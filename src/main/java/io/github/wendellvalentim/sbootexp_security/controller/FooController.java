3813dcf324e5163d23fbdd4548f412daf145997e
Wendell Valentim <wendellldasiilva8@gmail.com>
2026-04-01 17:50:23 -0300

    feat: implement basic security with in-memory authentication

---
+++ src/main/java/io/github/wendellvalentim/sbootexp_security/controller/FooController.java
@@ -0,0 +1,21 @@
+package io.github.wendellvalentim.sbootexp_security.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FooController {
+
+    @GetMapping("/public")
+    public ResponseEntity<String> publicRoute(){
+        return ResponseEntity.ok("Public Route, Ok!");
+    }
+
+    @GetMapping("/private")
+    public ResponseEntity<String> privateRoute(Authentication authentication){
+        System.out.println(authentication.getClass());
+        return ResponseEntity.ok("Private Route, Ok!, Usuario:" + authentication.getName());
+    }
+}
